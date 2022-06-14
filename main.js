b34e37330b0e8a47a355ff490fd59a90b4aab420
CRISTIAN CAETANO DA SILVA <u21724@LAPA12.academico.cotuca.unicamp.br>
2022-06-14 17:59:56 -0300

    Deploy V1.0.2c

--- main.js
+++ main.js
@@ -14,5 +14,21 @@ app.get('/', function(req,res){
     res.sendFile(path.join(__dirname + '/index.html'))
 })
 
+app.post('/api', (req, res) => {
+    console.log('[200]Post:\n', req.body)
+
+    _key = req.body.key
+    _key2 = req.body.key2
+
+    res.status(200).send(
+        { 
+            success : 'true',
+            message: 'WORKING' 
+        }
+    )
+})
+
+
+
 //app.listen(port, () => console.log('Funcionando na porta ${port}'))
 app.listen(port, () => console.log(`Funcionando na porta ${port}`))
